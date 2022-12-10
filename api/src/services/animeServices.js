d7a897af7ac35eab8f858cbe655767586d0cc184
Your Name <you@example.com>
2022-12-09 19:16:22 -0500

    Add /animes/:id route + query for /animes

--- api/src/services/animeServices.js
+++ api/src/services/animeServices.js
@@ -1,13 +1,16 @@
 const utils = require('../utils/utils');
 const { Anime, Genre } = require('../db.js');
+const { Op } = require('sequelize');
 
 exports.fillAnimeModel = async () => {
+
   let options = {
     include: [{
       model: Genre,
       through: {attributes: []},
       
     },],
+
     order: [['id', 'asc'], [Genre, 'id', 'asc']]
   }
 
@@ -19,7 +22,8 @@ exports.fillAnimeModel = async () => {
 
     } else {
       const allAnimes = await utils.getAllAnime();
-      console.log('NUMERO DE ANIMES:', allAnimes.length)
+      console.log('NUMERO DE ANIMES:', allAnimes.length);
+
       await Promise.all(allAnimes.map(async anime => {
         const auxGenres = anime.genres;
         delete anime.genres;
@@ -36,3 +40,65 @@ exports.fillAnimeModel = async () => {
     return "ESTO ES INFO" + error.message;
   }
 };
+
+exports.get_animes_by_query = async (query) => {
+  
+  await this.fillAnimeModel();
+  let limit = query.limit ? query.limit: 9
+  let page = query.page ? query.page: 1;
+  query.page && delete query.page;
+   // allow pass multiple genre params => Comedy, Action,...
+  let genres = query.genres ? {name: query.genres.split(",")}: {};
+  query.genres && delete query.genres;
+
+  query.name ? query.name = {
+    [Op.iLike]: `%${query.name}%`
+  } : {};
+ 
+  // console.log(query)
+  let options = {
+    where: query,
+    
+    include: [{
+      model: Genre,
+      through: {attributes: []},
+      where: genres
+    },],
+    order: [['id', 'asc'], [Genre, 'id', 'asc']],
+    limit: limit,
+    offset: limit * (page - 1)
+  }
+
+  try {
+    let dbData = await Anime.findAll(options)
+    // db con info
+    if(dbData) return dbData;
+    else throw new Error('Nooo');
+
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+exports.get_anime_by_id = async (id) => {
+  let options = {
+    include: [{
+      model: Genre,
+      through: {attributes: []},
+      
+    },],
+    order: [['id', 'asc'], [Genre, 'id', 'asc']]
+  }
+
+  try {
+    let anime = await Anime.findByPk(id, options);
+
+    if(anime.name) {
+      return anime;
+    } else {
+      throw new Error(`The anime with id ${id} does not exist`);
+    }
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
