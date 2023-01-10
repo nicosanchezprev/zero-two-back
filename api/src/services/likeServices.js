435217a83dc0270b193d495c977d2b41d1c5fd8e
Your Name <you@example.com>
2023-01-10 07:58:57 -0500

    add comments logic v2

---
+++ api/src/services/likeServices.js
@@ -0,0 +1,36 @@
+
+const {Like, User, Review} = require('../db.js');
+
+exports.changeLikeStatus = async (userId, commentId) => {
+    const likeToChange = await Like.findOne({where: {userId: userId, reviewId: commentId}})
+    const user = await User.findByPk(userId);
+    const commentToChangeLike = await Review.findByPk(commentId);
+      try {
+    
+        if (!commentToChangeLike) return('Comment not founded');
+        else {
+            let like;
+            if(!likeToChange) {
+                like = await Like.create();
+                like.created = !like.created;
+                await like.save();
+                await like.setUser(user);
+                await like.setReview(commentToChangeLike)
+                await like.save();
+
+                return ('Like change and created')
+                
+            } else {
+                console.log(likeToChange)
+                likeToChange.created = !likeToChange.created;
+                await likeToChange.save();
+
+                return('Like changed');
+            }
+            
+        }
+      } catch (err) {
+        console.log(err)
+        throw new Error('Like could not be modify')
+      }
+}
--- api/src/services/reviewServices.js
+++ api/src/services/reviewServices.js
@@ -1,5 +1,5 @@
 const utils = require('../utils/utils');
-const {Review, User} = require('../db.js');
+const {Review, User, Like} = require('../db.js');
 const { where, Op } = require('sequelize');
 
 exports.fillReviewModel = async (id) => {
@@ -66,8 +66,8 @@ exports.addReply= async (reply, nickname, episodeId,  commentId) => {
 try {
     const user = await User.findOne({where:{nickname: nickname}})
     const commentToReply = await Review.findOne({where: {id: commentId}})
-    console.log(episodeId, commentId)
-    console.log(commentToReply)
+    // console.log(episodeId, commentId)
+    // console.log(commentToReply)
     // console.log(user)
     if(!user) {
       throw new Error(`Please login to post a comment`)
@@ -76,9 +76,14 @@ try {
 
       const replyToAdd = await Review.create(reply);
       await replyToAdd.setUser(user);
-      await commentToReply.addReply(replyToAdd);
 
-      console.log(nickname)
+      replyToAdd.parent_id = commentToReply.parent_id === null ? commentToReply.id : commentToReply.parent_id;
+      await replyToAdd.save()
+      await commentToReply.addReply(replyToAdd);
+      replyToAdd.reply_id= commentToReply.parent_id === null ? commentToReply.id : commentToReply.parent_id;
+      await replyToAdd.save()
+      // console.log('a', replyToAdd)
+      // console.log(nickname)
       let replyCreated = await Review.findOne({where: {
         id: replyToAdd.id
         },
@@ -87,7 +92,7 @@ try {
         {model: Review, as: 'Replies'},
         ]
         })
-
+      
       return replyCreated
     }
     
@@ -103,7 +108,10 @@ exports.getEpisodeComments = async (episodeId) => {
       try {
       
         const comments = await Review.findAll({where: {
-          id_episode: episodeId}, 
+          id_episode: episodeId, 
+         replyingTo: {
+              [Op.is]: null
+            },}, 
            include: [
             {
               model: User,
@@ -112,22 +120,79 @@ exports.getEpisodeComments = async (episodeId) => {
             {
             model: Review, as: 'Replies',
             required: false,
+            
             // include: { all: true, nested: true },
             where: {
             // replyingTo: {
             //   [Op.not]: null
             // },
-            id_episode: episodeId
+            id_episode: episodeId,
+            parent_id : {[Op.not]: null}
             },
             include: 
             {
               model: User,
               attributes: ['nickname']
-            },}
-          ]
+            },},
+            {
+              model: Like,
+  
+            },
+            
+          ],
+          // group: ['Like.reviewId'],
+          // raw:true,
+          order: [['id', 'desc'], ['Replies', 'id', 'asc']]
+          // nested:true
         });
-        return comments;
+        let auxComments;
+        auxComments = await comments.map(comment => {
+          let likesLength = comment.dataValues.likes.filter(like => like.created === true).length;
+          console.log(likesLength)
+          comment.dataValues.likes = likesLength;
+          return comment;
+        } )
+        // comments.forEach(comment => comment.likes = comment.likes.length)
+        return auxComments;
+        
       } catch (error) {
+        console.log(error)
         throw new Error(error.message);
       }
+}
+
+exports.deletePost = async(episodeId, commentId) => {
+  try {
+    console.log(commentId)
+    let commentToDelete = await Review.findOne({where: {id: commentId, id_episode: episodeId}});
+    if(!commentToDelete) {
+      throw new Error(`The comment wasn't founded`);
+    }
+    else {
+      await Review.destroy({where: {id: commentId}});
+      return 'Deleted';
+    }
+  } catch (err) {
+    console.log(err.message)
+    throw new Error(err.message);
+  }
+}
+
+exports.editPost = async(episodeId, commentId, content) => {
+  try {
+    console.log(commentId)
+    let commentToEdit = await Review.findOne({where: {id: commentId, id_episode: episodeId}});
+    if(!commentToEdit) {
+      throw new Error(`The comment can't be edited`);
+    }
+    else {
+      console.log(content)
+      commentToEdit.content = content
+      await commentToEdit.save();
+      return 'Comment edited successfully!'
+    }
+  } catch (err) {
+    console.log(err.message)
+    throw new Error(err.message);
+  }
 }
